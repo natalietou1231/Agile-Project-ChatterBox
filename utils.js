d0da01cac69c386283cdad83614773e847a5befd
natalietou1231 <48970071+natalietou1231@users.noreply.github.com>
2019-05-07 10:22:46 -0700

    passport-local

--- utils.js
+++ utils.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 module.exports.init = function(){
     mongoose.connect('mongodb+srv://admin:112233444@cluster0-om2ow.mongodb.net/chatroom', { useNewUrlParser: true } );
     var db = mongoose.connection;
+    var path = require('path');
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function callback () {
         console.log('Successfully connected to MongoDB server');
