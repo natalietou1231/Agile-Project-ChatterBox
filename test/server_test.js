db6471f736a5a8673aa9d45f7442e6bc31b3ad03
oliviapangkiey <oliviapangkiey07@gmail.com>
2019-05-17 13:11:26 -0700

    Lasttest

--- test/server_test.js
+++ test/server_test.js
@@ -49,210 +49,6 @@ describe("SAMPLE unit test",function(){
 
         chai.request("http://localhost:8080")
             .post("/login")
-            .send({username: "OO", password:"Kucing07!"})
-
-            .end((err,res)=>{
-                should.exist(res.body);
-                res.should.redirectTo("http://localhost:8080/chatroom");
-                done();
-            });
-    });
-
-    it("should not login to chatroom", (done)=>{
-
-        chai.request("http://localhost:8080")
-            .post("/login")
-            .send({username: "rawr", password:"Kucing07!"})
-            .end((err,res)=>{
-                should.exist(res.body);
-                res.should.redirectTo("http://localhost:8080/login/incorrect");
-                done();
-            });
-    });
-
-    it("should sign up a user", (done)=>{
-        let json ={};
-        json.username= "ddd";
-        json.password = "11111Rf";
-        json.first_name = "aaa";
-        json.last_name = "ddd";
-        json.email = "111@g.com";
-        json.registration_date = new Date();
-
-        chai.request("http://localhost:8080")
-            .post("/signup")
-            .send(json)
-            .end((err,res)=>{
-                should.exist(res.body);
-                res.should.have.status(200);
-                done();
-            });
-
-    });
-
-    it("should not create user if email exists", (done)=>{
-        let json ={};
-        json.username= "ddd";
-        json.password = "11111Rf";
-        json.first_name = "aaa";
-        json.last_name = "ddd";
-        json.email = "aa@1";
-        json.registration_date = new Date();
-
-        chai.request("http://localhost:8080")
-            .post("/signup")
-            .send(json)
-            .end((err,res)=>{
-                should.exist(res.body);
-                res.should.redirectTo("http://localhost:8080/signup/exists");
-                done();
-            });
-
-    });
-
-    it("should not create user if username exists", (done)=>{
-        let json ={};
-        json.username= "eee";
-        json.password = "11111Rf";
-        json.first_name = "aaa";
-        json.last_name = "ddd";
-        json.email = "aa@1";
-        json.registration_date = new Date();
-
-        chai.request("http://localhost:8080")
-            .post("/signup")
-            .send(json)
-            .end((err,res)=>{
-                should.exist(res.body);
-                res.should.redirectTo("http://localhost:8080/signup/exists");
-                done();
-            });
-
-    });
-
-    it("should get chatroom", (done)=>{
-        chai.request("http://localhost:8080")
-            .get("/chatroom")
-            .end((err,res)=>{
-                should.exist(res.body);
-                res.should.have.status(200);
-                done();
-            });
-    });
-    /* ---------------------------Deliverable 1 Tests----------------*/
-
-    var agent = chai.request.agent("http://localhost:8080");
-    it("should get user account information", (done)=>{
-        agent
-            .post("/login")
-            .send({_method:"post", username: "www", password:"111111Rf"})
-            .then(function(){
-                // res.should.have.cookie('sessionid');
-                return agent.get('/account')
-                    .then(function (res) {
-                        expect(res).to.have.status(200);
-                        done();
-                    });
-            })
-        // agent.close()
-    });
-
-    it("should update profile", (done)=>{
-        agent
-            .post("/login")
-            .send({_method:"post", username: "www", password:"111111Rf"})
-            .then(function(){
-                return agent.post('/account/update-form')
-                    .send({last_name:"Olivia",
-
-                        first_name:"Olivia",
-                        username:"www111",
-                        password:"111111Rf",
-                        email: "2@eer"})
-                    .then(function(){
-                        return agent.get('/account')
-                            .then(function (res) {
-                                expect(res).to.have.status(200);
-                                assert.equal(res.body.user[0].username,'www111');
-                                assert.equal(res.body.user[0].name,'OliviaOlivia');
-                                assert.equal(res.body.user[0].email,'2@eer');
-                                done()
-
-                            });
-
-                    })
-
-
-            })
-    });
-    var agent = chai.request.agent("http://localhost:8080")
-    it("should log out", (done)=> {
-        agent
-            .post("/login")
-            .send({username: "OO", password: "Kucing07!"})
-            .then(() => {
-                return agent.get('/logout')
-                    .then((err, res) => {
-                        expect(res).to.have.status(200);
-                        expect(res).to.redirectTo("http://localhost:8080");
-                        done()
-                    });
-            })
-    })
-
-                var agent = chai.request.agent("http://localhost:8080")
-                it("should NOT update profile", (done)=>{
-                    agent
-                        .post("/login")
-                        .send({_method:"post", username: "chowzler", password:"Asdf1234"})
-                        .then(function(res){
-                            return agent.get('/account/update')
-                                .send({
-                                    _method:"post",
-                                    last_name:"Olivia",
-                                    first_name:"Olivia",
-                                    username:"chowzler",
-                                    password:"Asdf1234",
-                                    email: "a@gmail.com"})
-                                .then(function(){
-                                    return agent.get('/account')
-                                        .then(function (res) {
-                                            expect(res).to.have.status(200);
-                                            done()
-                                        });
-
-                                })
-
-
-                        })
-                });
-    //
-    // it("should return home page", (done)=>{
-    //     chai.request("http://localhost:8080")
-    //         .get("/")
-    //         .end((err,res)=>{
-    //             should.exist(res.body);
-    //             res.should.have.status(200);
-    //             done();
-    //         });
-    // });
-    // it("should not have access to chatroom", (done)=>{
-    //
-    //     chai.request("http://localhost:8080")
-    //         .post("/login")
-    //         .send({username: "op12355", password:"Kucing07!"})
-    //         .end((err,res)=>{
-    //             // should.exist(res.body);
-    //             res.should.redirectTo("http://localhost:8080/login/incorrect");
-    //             done();
-    //         });
-    // });
-
-
-    it("should login to chatroom", (done)=>{
-
-        chai.request("http://localhost:8080")
-            .post("/login")
             .send({_method: "post", username: "tester1", password:"Asdf1234"})
             .end((err,res)=>{
                 should.exist(res.body);
@@ -281,25 +77,25 @@ describe("SAMPLE unit test",function(){
     //         });
     //
     // });
-       it("should not create user if email exists", (done)=>{
-            let json ={};
-            json.username= "zxcvbnmz";
-            json.password = "11111Rf";
-            json.first_name = "aaa";
-            json.last_name = "ddd";
-            json.email = "111@gmail.comqqq";
-            json.registration_date = new Date();
+    it("should not create user if email exists", (done)=>{
+        let json ={};
+        json.username= "zxcvbnmz";
+        json.password = "11111Rf";
+        json.first_name = "aaa";
+        json.last_name = "ddd";
+        json.email = "111@gmail.comqqq";
+        json.registration_date = new Date();
 
-            chai.request("http://localhost:8080")
-                .post("/signup")
-                .send(json)
-                .end((err,res)=>{
-                    should.exist(res.body);
-                    res.should.redirectTo("http://localhost:8080/signup/exists");
-                    done();
-                });
+        chai.request("http://localhost:8080")
+            .post("/signup")
+            .send(json)
+            .end((err,res)=>{
+                should.exist(res.body);
+                res.should.redirectTo("http://localhost:8080/signup/exists");
+                done();
+            });
 
-        });
+    });
 
     it("should not create user if username exists", (done)=>{
         let json ={};
@@ -339,7 +135,9 @@ describe("SAMPLE unit test",function(){
     it("should get user account information", (done)=>{
         agent
             .post("/login")
-            .send({'local.username': "OO", 'local.password':"Kucing07!"})
+            .send({_method:"post", username: "tester1", password:"Asdf1234"})
+
+
             .then(function(){
                 // res.should.have.cookie('sessionid');
                 return agent.get('/account')
@@ -420,33 +218,27 @@ describe("SAMPLE unit test",function(){
                     })
             })
     });
-    var agent = chai.request.agent("https://afternoon-mesa-87220.herokuapp.com/");
+    var brows = chai.request.agent("https://afternoon-mesa-87220.herokuapp.com/");
     it("should be able to sign in with facebook", (done)=>{
-        agent
-            .post("/auth/facebook")
-            .send({_method:"post", email: "tester_nuqpmiv_one@tfbnw.net", password:"111111Rf"})
+        brows
+            .get("/auth/facebook")
+            // .send({email: "tester_nuqpmiv_one@tfbnw.net", password:"111111Rf"})
             .then(function(){
+                // res.should.redirectTo("https://afternoon-mesa-87220.herokuapp.com/chatroom");
+                // done();
                 // res.should.have.cookie('sessionid');
-                return agent.get('/chatroom')
-                    .then(function (res) {
+                return brows.get('chatroom')
+                    .then(function (res, err) {
                         expect(res).to.have.status(200);
                         done();
-                    });
+                    })
+                    // .catch(function(err){
+                    //     console.log(err)
+                    //     done()
+                    // })
             })
     });
 
-
-})
-
-
-
-
-
-
-
-
-
-
-      
+});
 
 
